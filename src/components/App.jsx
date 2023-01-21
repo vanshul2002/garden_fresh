fc1c09501e500e24d17704106dbb94510a7017aa
priyanshu <priyanshuraja456@gmail.com>
2023-01-22 03:09:56 +0530

    changed home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './header';
+import Hero from './hero';
 function App(){
     return (
      <div>
        <Header/>
+       <Hero/>
     </div>
     )
 }
---
+++ src/components/hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+function Hero(){
+    return(
+    <div className='flex ml-52 mt-28  '>
+     <h1 className='font-sans text-[#323557] text-5xl'>
+        Garden Fresh
+        <p className='font-sans text-base ml-2 mt-4 text-[#a6acc6]'> <p>If you want to get some fresh food</p><p>reach us</p> </p>
+        <button class="bg-[#2bc6eb] text-sm ml-2 mt-2 hover:bg-blue-400 text-[#323557] font-bold py-2 px-10 rounded">
+  Buy
+</button>
+     </h1>
+    
+    </div>
+   
+
+    )
+}
+  
+export default Hero
