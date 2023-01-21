81740c1d491d44b8027b5b65f09b1715600812a7
priyanshu <priyanshuraja456@gmail.com>
2023-01-22 00:57:40 +0530

    nav_bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css"
+import App from "./components/App.jsx"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-  </div>
+   <App className = "bg-[#f2eeeb]"/>
 );
 
